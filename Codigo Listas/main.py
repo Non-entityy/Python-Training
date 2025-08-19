09314d1fa625fe64dc18fb63cf75e2ca89bc4a0c
Non-entityy <vitornew.a@gmail.com>
2025-08-19 22:56:33 +0000

    Atualização 1.3 4º Aula

---
+++ Codigo Listas/main.py
@@ -0,0 +1,20 @@
+#frutas = ["Maçã","Uva","Kiwi",["Pêra","Tomate"],"Açai"]
+#frutas.sort()
+#frutas.sort(reverse = False OR True)
+#------------------------------------------------------------------------------------------
+#PRINTS
+
+#print(frutas)
+#print(frutas[:])
+#print(frutas[3])
+#print(frutas[3][0])
+
+#--------------------------------------------------------------------------------------------
+lista1 = []
+for i in range(10):
+    lista1.append(i+1)
+print(lista1)
+
+#lista2 = [i for i in range(10)]
+#lista2 = [[linha for linha in range(3)]for coluna in range(3)]
+#print(lista2)
---
+++ Fixação Listas & Copias/main.py
@@ -0,0 +1,9 @@
+#1. Criar uma lista com varios tipos de dados
+
+#2. Criar uma lista e adicione dois itens e remova 1
+# 
+#3. Faça uma cópia real de uma lista
+# 
+#4. Crie uma lista com numeros e multiplique cada numero por 5
+
+#5. Pegue a lista a1 e gere uma nova lista apenas com os valores 4 e 5 a1=[1,2,3,4,5,6] ====> [4,5]
