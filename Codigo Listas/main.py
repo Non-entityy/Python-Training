a6663b75d74a2ea9e54a84baf8bad649a1876cbc
Non-entityy <vitornew.a@gmail.com>
2025-08-28 22:35:51 +0000

    Consertando ás coisas

--- Codigo Listas/main.py
+++ Codigo Listas/main.py
@@ -18,3 +18,43 @@ print(lista1)
 #lista2 = [i for i in range(10)]
 #lista2 = [[linha for linha in range(3)]for coluna in range(3)]
 #print(lista2)
+#----------------------------------------------------------------------------------------------
+#Tuplas
+# t = (10, 20, 30, 40)
+
+# print(t[0])  # Saída: 10
+# print(t[2])  # Saída: 30
+
+# Concatenando tuplas
+# t1 = (1, 2)
+# t2 = (3, 4)
+# t3 = t1 + t2
+# print(t3)  # Saída: (1, 2, 3, 4)
+
+# # Repetindo tuplas
+# t4 = (5,) * 3
+# print(t4)  # Saída: (5, 5, 5)
+
+# # Contando elementos
+# t5 = (1, 2, 2, 3, 3, 3)
+# print(t5.count(3))  # Saída: 3
+
+# Tupla como chave de dicionário
+# d = { (1, 2): "a", (3, 4): "b" }
+# print(d[(1, 2)])  # Saída: a
+
+# # Tupla para lista
+# t = (1, 2, 3)
+# lista = list(t)
+# print(lista)  # Saída: [1, 2, 3]
+
+# # Lista para tupla
+# l = [4, 5, 6]
+# tupla = tuple(l)
+# print(tupla)  # Saída: (4, 5, 6)
+
+
+# t = (10, 20, 30, 20, 40)
+
+# pos = t.index(20)
+# print(pos)  # Saída: 1 (a primeira ocorrência de 20 está no índice 1)
