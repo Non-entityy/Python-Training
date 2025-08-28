ac8a26f148db38a0f25c5d837372c7235b9f6fdd
Non-entityy <vitornew.a@gmail.com>
2025-08-28 22:38:24 +0000

    esqueci disso kk

--- Codigo Listas/main.py
+++ Codigo Listas/main.py
@@ -10,10 +10,10 @@
 #print(frutas[3][0])
 
 #--------------------------------------------------------------------------------------------
-lista1 = []
-for i in range(10):
-    lista1.append(i+1)
-print(lista1)
+# lista1 = []
+# for i in range(10):
+#     lista1.append(i+1)
+# print(lista1)
 
 #lista2 = [i for i in range(10)]
 #lista2 = [[linha for linha in range(3)]for coluna in range(3)]
