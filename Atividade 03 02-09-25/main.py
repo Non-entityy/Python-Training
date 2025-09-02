98896448ee69c581d295943c7b13c5929fa87496
Non-entityy <vitornew.a@gmail.com>
2025-09-02 23:46:56 +0000

    Metade da atividade de funções feita

---
+++ Atividade 03 02-09-25/main.py
@@ -0,0 +1,40 @@
+#Exercícios de funções
+
+# 1. Crie uma função chamada saudacao que imprime na tela a mensagem "Olá, bem-vindo ao Python!". Em seguida, chame a função no programa.
+
+
+# def saudacao():
+#      print('Olá, bem-vindo ao Python!')
+
+# saudacao()
+
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# 2. Crie uma função chamada dobro que receb um número como parâmetro e retorna o dobro desse número. Teste chamando a função com diferentes valores.
+
+
+# def dobro(num):
+#     return num*2
+
+# print(dobro(2))
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# 3. Crie uma função chamada soma que receba dois números e retorne a soma deles. Depois, use a função para somar 10 + 20.
+
+# def soma(num1,num2):
+#     return num1+num2
+# print(soma(10,20))
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# 4. Crie uma função chamada mensagem que receba um nome como parâmetro e exiba a mensagem: "Olá,[nome]!"
+
+# def mensagem(nome):
+#     print(f'Olá, {nome}!')
+# mensagem("Lucas")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# 5.
