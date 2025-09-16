849b98f82a0027d392f48e2dae31a8b56facff93
Non-entityy <vitornew.a@gmail.com>
2025-09-16 19:07:09 +0000

    Finalizado atividade 06 de POO

--- Atividade 06 11-09-25/main.py
+++ Atividade 06 11-09-25/main.py
@@ -26,4 +26,32 @@
 
 # 2. Crie uma classe, Pessoa, que tenha os atributos: nome, data de nascimento, cpf, identidade. Deixe os atributos cpf e identidade como privado e monte os métodos setters e getters para acessar e editar os valores
 
-
+# class Pessoa:
+#     def __init__(self, nome, data, cpf, identidade):
+#         self.nome = nome
+#         self.data = data
+#         self.__cpf = self.validar(cpf)
+#         self.__identidade = identidade
+#     def __str__(self):
+#         return f'Nome: {self.nome} | Data de nascimento: {self.data}'
+#     def get_cpf(self):
+#         return self.__cpf
+#     def set_cpf(self, cpf):
+#         self.__cpf = self.validar(cpf)
+#     def get_identidade(self):
+#         return self.__identidade
+#     def set_identidade(self, identidade):
+#         self.__identidade = identidade
+#     def validar(self, cpf):
+#         cpf_str = str(cpf)
+#         # Validação simplificada (apenas checa o formato de 11 dígitos)
+#         if len(cpf_str) == 11 and cpf_str.isdigit():
+#             return cpf_str
+#         else:
+#             raise ValueError("CPF inválido. Deve conter 11 dígitos.")
+# pessoa = Pessoa('Lucas', '27/11/1997', 14736512413, 11560789)
+# print(pessoa)
+# print(f'CPF antes: {pessoa.get_cpf()}')
+# pessoa.set_cpf(12345678912)
+# print(f'CPF depois: {pessoa.get_cpf()}')
+      
