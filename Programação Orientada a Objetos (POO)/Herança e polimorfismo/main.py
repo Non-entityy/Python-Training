40a4383ce8ae56b547c1f7660dfa19d32672ddf1
Non-entityy <vitornew.a@gmail.com>
2025-09-22 20:07:07 +0000

    Quase terminado Herança

--- Programação Orientada a Objetos (POO)/Herança e polimorfismo/main.py
+++ Programação Orientada a Objetos (POO)/Herança e polimorfismo/main.py
@@ -3,27 +3,87 @@
 
 # 1. Crie uma classe Usuario com atributos nome e email. Depois, crie uma classe Cliente que herde de Usuario. Crie uma instancia de um cliente e acesse todos os atributos.
 
+# class Usuario:
+#     def __init__(self,nome,email):
+#         self.nome = nome
+#         self.email = email
 
+# class Cliente(Usuario):
+#     def __str__(self):
+#         return f'\nDados do Cliente: \nNome: {self.nome}\nEmail: {self.email}'
 
+# cliente = Cliente('Lucas', 'Lucasp@gmail.com')
+# print(cliente)
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # 2. Implemente um método exibir_informacoes() na classe Usuario e herde esse método em Cliente. Mostre como chamar o método herdado a partir de um objeto Cliente.
 
+# class Usuario:
+#     def __init__(self,nome,email):
+#         self.nome = nome
+#         self.email = email
+
+#     def exibir_informacoes(self):
+#         return f'\nDados do Cliente \nNome: {self.nome}\nEmail: {self.email}'
+
+# class Cliente(Usuario):
+#     pass
+
+
+# cliente= Cliente('Lucas', 'Lucasp@gmail.com')
+# print(cliente.exibir_informacoes())
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 3. Na classe Usuario, crie um método saudacao() que retorna "Olá, usuário". Na classe Cliente, sobrescreva esse método para retornar "Olá, cliente". Mostre como funciona a chamada.
 
+# class Usuario:
+#     def __init__(self,nome,email):
+#         self.nome = nome
+#         self.email = email
 
+#     def __str__(self):
+#         return f'\n|Seus Dados|\nNome: {self.nome}\nEmail: {self.email}'
+
+#     def saudacao(self):
+#         return f'Olá, usuário.'
+# class Cliente(Usuario):
+#     def saudacao(self):
+#         return f'Olá, cliente.'
+# cliente= Cliente('Lucas', 'Lucasp@gmail.com')
+# print(cliente.saudacao())
+        
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 4. Na classe Cliente, adicione o atributo saldo. Adicione um método construtor em Cliente que inicialize também os atributos de Usuario usando super().
 
 
+# class Usuario:
+#     def __init__(self,nome,email):
+#         self.nome = nome
+#         self.email = email
+
+#     def __str__(self):
+#         return f'\n|Seus Dados|\n Nome: {self.nome}\n Email: {self.email}'
+
+# class Cliente(Usuario):
+#     def __init__(self, nome, email, saldo):
+#         super().__init__(nome, email)
+#         self.saldo = saldo
+#     def exibir_saldo(self):
+#         return f'Saldo: R$ {self.saldo:.2f}'
+
+
+# cliente= Cliente('Lucas', 'Lucasp@gmail.com', 500)
+# print(cliente.exibir_saldo())
+# print(cliente)
+        
+
+
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # 5. Crie uma classe Funcionario que herda de Usuario e, em seguida, crie uma classe Gerente que herda de Funcionario. Mostre como instanciar um gerente e acessar seus atributos.
-
+   
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
