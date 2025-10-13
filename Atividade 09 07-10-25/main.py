3c9a92bd5fc913b63b2a37dfc957234163227058
Non-entityy <vitornew.a@gmail.com>
2025-10-13 02:43:28 +0000

    Terminando Atividade 09 POO - Classes abstratas

--- Atividade 09 07-10-25/main.py
+++ Atividade 09 07-10-25/main.py
@@ -1,5 +1,5 @@
 # Atividade: POO - Classes abstratas
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 
 # 1. Definição de classe abstrata
 # Crie uma classe abstrata chamada Animal que possua um método abstrato falar(). Em seguida, crie duas classes concretas (Cachorro e Gato) que implementem esse método. Mostre o uso criando objetos e chamando o método falar().
